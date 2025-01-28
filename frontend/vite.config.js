4fb7b632edd32135c20e54f3c609904b3653126e
rexwithluv <aaronaax@gmail.com>
2025-01-29 00:44:48 +0100

    chore(env): add dotenv y configuradas variables de entorno en vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,22 @@
-import { fileURLToPath, URL } from 'node:url'
+/* eslint-disable no-undef */
+// Usamos process.env porque estamos en el back, pero igualmetne eslint lo marca como error
+// En el front (contexto de navegador) se debe usar import.meta.env
 
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import dotenv from 'dotenv'
+import { resolve, dirname } from "path"
 
-// https://vite.dev/config/
+// Cargar variables de entorno desde un directorio superior
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+dotenv.config({
+  path: resolve(__dirname, "../.env"),
+})
+
 export default defineConfig({
   plugins: [
     vue(),
@@ -14,5 +26,9 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
-  }
+  },
+  server: {
+    host: true,
+    allowedHosts: process.env.VITE_ALLOWED_HOSTS.split(",") || "localhost",
+  },
 })
