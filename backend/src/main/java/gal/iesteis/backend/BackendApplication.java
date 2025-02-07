b808688d11155d52173e5c5bcb081870d67879b4
rexwithluv <aaronaax@gmail.com>
2025-02-07 19:31:57 +0100

    feat(backend): creado backend
    creada la estructura de directorios y archivos de backend en springboot

---
+++ backend/src/main/java/gal/iesteis/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package gal.iesteis.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/gal/iesteis/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package gal.iesteis.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
