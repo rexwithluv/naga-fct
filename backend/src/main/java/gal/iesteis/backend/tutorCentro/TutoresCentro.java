d1a9c4f73daf88f50978460af6792b2bc2fbe2a1
rexwithluv <aaronaax@gmail.com>
2025-03-23 12:17:12 +0100

    entidad TutorCentro

---
+++ backend/src/main/java/gal/iesteis/backend/tutorCentro/TutoresCentro.java
@@ -0,0 +1,40 @@
+package gal.iesteis.backend.tutorCentro;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "tutores_centro")
+public class TutoresCentro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nombre", length = 500, nullable = false)
+    private String nombre;
+
+    @Column(name = "apellidos", length = 500, nullable = false)
+    private String apellidos;
+
+    @Column(name = "email", length = 500, nullable = false)
+    private String email;
+
+    @Column(name = "curso_id", nullable = false)
+    private Short cursoId;
+
+    @Column(name = "activo", nullable = false)
+    private Boolean activo;
+}
