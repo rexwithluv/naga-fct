f24fb160d06293444b3dd196af36eb7c999da947
rexwithluv <aaronaax@gmail.com>
2025-03-23 12:28:16 +0100

    entidad AlumnosEmpresas

---
+++ backend/src/main/java/gal/iesteis/backend/alumnosEmpresas/AlumnosEmpresas.java
@@ -0,0 +1,39 @@
+package gal.iesteis.backend.alumnosEmpresas;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "alumnos_empresas")
+public class AlumnosEmpresas {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "alumno_id", nullable = false)
+    private Long alumnoId;
+
+    @Column(name = "empresa_id", nullable = false)
+    private Long empresaId;
+
+    @Column(name = "fecha_inicio", nullable = false)
+    private LocalDate fechaInicio;
+
+    @Column(name = "fecha_fin")
+    private LocalDate fechaFin;
+}
