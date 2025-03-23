26d25a3055f650976347bf7ccd646952f60c9ff7
rexwithluv <aaronaax@gmail.com>
2025-03-23 11:24:48 +0100

    entidad EstadosAlumno

---
+++ backend/src/main/java/gal/iesteis/backend/estadoAlumno/EstadosAlumno.java
@@ -0,0 +1,27 @@
+package gal.iesteis.backend.estadoAlumno;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "estados_alumnos")
+public class EstadosAlumno {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(name = "nombre", length = 500, nullable = false)
+    String nombre;
+}
