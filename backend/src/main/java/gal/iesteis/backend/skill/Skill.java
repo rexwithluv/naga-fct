de3d978b9947d63e78efd83f423cbf01e45a41a6
rexwithluv <aaronaax@gmail.com>
2025-03-23 12:10:43 +0100

    entidad Skill

---
+++ backend/src/main/java/gal/iesteis/backend/skill/Skill.java
@@ -0,0 +1,30 @@
+package gal.iesteis.backend.skill;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "skills")
+public class Skill {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nombre", length = 500, nullable = false)
+    private String nombre;
+
+    @Column(name = "especialidad_id", nullable = false)
+    private Byte especialidadId;
+}
