e32d815951b9f1a285ffb6384063c8fd1105eded
rexwithluv <aaronaax@gmail.com>
2025-03-24 08:57:35 +0100

    feat(frontend): el header del sitio web no se mostrará en páginas como login

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,22 +4,7 @@ import AppHeader from './components/AppHeader.vue';
 
 <template>
   <div>
-    <AppHeader />
-
-    <!-- <header>
-      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-      <div class="wrapper">
-        <HelloWorld msg="You did it!" />
-
-        <nav>
-          <RouterLink to="/">Home</RouterLink>
-          <RouterLink to="/about">About</RouterLink>
-        </nav>
-      </div>
-    </header>
-
-    <RouterView /> -->
+    <AppHeader v-if="$route.path !== '/login' || $route.path !== '/home'" />
+    <RouterView />
   </div>
 </template>
-
