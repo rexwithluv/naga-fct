4b0975a2eef646906d276ff422fe6a5ffb0e5dd4
rexwithluv <aaronaax@gmail.com>
2025-03-28 19:58:45 +0100

    feat(backend): se ha creado la clase ModelMapperConfig para comenzar a trabajar con DTOs

---
+++ backend/src/main/java/gal/iesteis/backend/config/modelMapper/ModelMapperConfig.java
@@ -0,0 +1,14 @@
+package gal.iesteis.backend.config.modelMapper;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
