c5e62b23afb912ada2cd51698fbc6f3f66672e0f
rexwithluv <aaronaax@gmail.com>
2025-03-29 13:40:16 +0100

    chore(db): se ha cambiado el nombre de las FK de usuario para ser más descriptivas

--- db/schema.sql
+++ db/schema.sql
@@ -145,10 +145,10 @@ ALTER TABLE tutores_centro
 ADD CONSTRAINT fk_tutor_centro_curso FOREIGN KEY (curso_id) REFERENCES cursos (id);
 
 ALTER TABLE usuarios
-ADD CONSTRAINT fk_usuarios_rol FOREIGN KEY (rol_id) REFERENCES roles_usuarios (id);
+ADD CONSTRAINT fk_usuario_rol FOREIGN KEY (rol_id) REFERENCES roles_usuarios (id);
 
 ALTER TABLE usuarios
-ADD CONSTRAINT fk_usuarios_tutor FOREIGN KEY (tutor_id) REFERENCES tutores_centro (id);
+ADD CONSTRAINT fk_usuario_tutor FOREIGN KEY (tutor_id) REFERENCES tutores_centro (id);
 
 ALTER TABLE fct
 ADD CONSTRAINT fk_fct_alumno FOREIGN KEY (alumno_id) REFERENCES alumnos (id) ON DELETE CASCADE;
