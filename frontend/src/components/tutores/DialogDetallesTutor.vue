e7e76101fceb57a100064a2af895b31dc262f593
rexwithluv <aaronaax@gmail.com>
2025-04-03 20:30:33 +0200

    feat(frontend): ahora podemos ver los detalles de cada tutor del centro

---
+++ frontend/src/components/tutores/DialogDetallesTutor.vue
@@ -0,0 +1,45 @@
+<script setup lang="js">
+import apiClient from '@/apiClient'
+import { useToast } from 'primevue'
+import { watch, ref } from 'vue'
+
+const toast = useToast()
+const tutorID = defineModel('tutorID')
+const visible = defineModel('visible')
+
+const tutor = ref(null)
+
+const getTutorData = async () => {
+    try {
+        const response = await apiClient.get(`/tutores-centro/${tutorID.value}`)
+        tutor.value = response.data
+    } catch (error) {
+        toast.add({
+            severity: 'error',
+            summary: 'Error al cargar la/el tutora/',
+            detail: error.message,
+            life: 5000,
+        })
+    }
+}
+
+// Solo cuando el Dialog es visible intentamos cargar los datos
+watch(visible, async (newValue) => {
+    if (newValue === true) {
+        await getTutorData()
+    }
+})
+</script>
+
+<template>
+    <Dialog v-model:visible="visible" header="Detalles de tutora/" modal dismissableMask>
+        <ul>
+            <li>ID: {{ tutor?.id }} </li>
+            <li>Nombre: {{ tutor?.nombre }}</li>
+            <li>Apellidos: {{ tutor?.apellidos }}</li>
+            <li>Correo electrónico: {{ tutor?.email }}</li>
+            <li>Curso: {{ tutor?.curso }}</li>
+            <li>Activo: {{ tutor?.activo }}</li>
+        </ul>
+    </Dialog>
+</template>
--- frontend/src/views/TutoresPage.vue
+++ frontend/src/views/TutoresPage.vue
@@ -1,9 +1,46 @@
 <script setup lang="js">
-import TutoresTabla from '@/components/tutores/TutoresTabla.vue'
+import apiClient from '@/apiClient'
+import DialogDetallesTutor from '@/components/tutores/DialogDetallesTutor.vue'
+import { useToast } from 'primevue/usetoast'
+import { onMounted, ref } from 'vue'
+
+const toast = useToast()
+const tutores = ref([])
+const tutorID = ref(null)
+const dialogDetalles = ref(false)
+
+const getTutores = async () => {
+  try {
+    const response = await apiClient.get('/tutores-centro')
+    tutores.value = response.data
+  } catch (error) {
+    toast.add({
+      severity: 'error',
+      summary: 'Error al cargar los tutores',
+      detail: error.message,
+      life: 5000,
+    })
+  }
+}
+
+const verDetalles = (e) => {
+  tutorID.value = e.data.id
+  dialogDetalles.value = true
+}
+
+onMounted(getTutores)
 </script>
 
 <template>
   <div>
-    <TutoresTabla />
+    <DialogDetallesTutor v-model:tutorID="tutorID" v-model:visible="dialogDetalles" />
+
+    <DataTable :value="tutores" @row-click="verDetalles" row-hover>
+      <Column field="nombre" header="Nombre" />
+      <Column field="apellidos" header="Apellidos" />
+      <Column field="email" header="Email" />
+      <Column field="curso" header="Curso" />
+      <Column field="activo" header="Activo" />
+    </DataTable>
   </div>
 </template>
