dcc26ef665300aee00d917067f37ce34bceb9656
rexwithluv <aaronaax@gmail.com>
2025-04-05 16:43:14 +0200

    refactor(frontend): eliminado componente AppHeader innecesario

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,11 @@
 <script setup lang="js">
-import AppHeader from './components/AppHeader.vue';
+import AppNavBar from './components/AppNavBar.vue';
 </script>
 
 <template>
   <div class="flex flex-col h-screen">
     <Toast />
-    <AppHeader v-if="$route.path !== '/' && $route.path !== '/login'" />
+    <AppNavBar v-if="$route.path !== '/' && $route.path !== '/login'" />
 
     <RouterView class="flex-1" />
 
--- frontend/src/components/AppHeader.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="js">
-import NavBar from './NavBar.vue';
-</script>
-
-<template>
-    <header>
-        <NavBar />
-    </header>
-</template>
