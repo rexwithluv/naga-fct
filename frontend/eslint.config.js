509caa0a85ee69deb2c5c5b08cdff7b903a9da59
rexwithluv <aaronaax@gmail.com>
2025-04-12 17:47:19 +0200

    chore(frontend): migración a ts, adaptamos convenciones y buenas prácticas

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,19 +1,30 @@
 import js from '@eslint/js'
 import pluginVue from 'eslint-plugin-vue'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import tseslint from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
 
 export default [
   {
     name: 'app/files-to-lint',
-    files: ['**/*.{js,mjs,jsx,vue}'],
+    files: ['**/*.{js,mjs,jsx,ts,tsx,vue}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+        ecmaVersion: 2022,
+        sourceType: 'module',
+      },
+    },
   },
 
   {
     name: 'app/files-to-ignore',
-    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+    ignores: ['**/dist/**', '**/coverage/**', '**/.output/**'],
   },
 
   js.configs.recommended,
   ...pluginVue.configs['flat/essential'],
+  tseslint.configs.recommended,
   skipFormatting,
 ]
