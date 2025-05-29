67ab59a5f98551d2fd1ff56ee21d59cd5b2ec4d3
rexwithluv <aaronaax@gmail.com>
2025-05-29 23:37:09 +0200

    refactor(backend): RFC 9457 sobre el GlobalControllerAdvice - GlobalExceptionHandler

--- backend/src/main/java/gal/iesteis/backend/config/controllerAdvice/GlobalExceptionHandler.java
+++ backend/src/main/java/gal/iesteis/backend/config/controllerAdvice/GlobalExceptionHandler.java
@@ -1,7 +1,11 @@
 package gal.iesteis.backend.config.controllerAdvice;
 
+import java.net.URI;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -16,67 +20,126 @@ import gal.iesteis.backend.tutorCentro.TutorCentroNotFoundException;
 import gal.iesteis.backend.usuario.UsuarioForbiddenException;
 import gal.iesteis.backend.usuario.UsuarioNotFoundException;
 
+// RFC 9457 - Estándar moderno sobre el formato de los problemas HTTP, actualización del RFC 7807
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    // Lo ideal sería tener una URL navegable desde un navegador donde podamos
+    // específicar los problemas con contexto y explicación para un usuario común.
+    private static final String BASE_URL = "https://naga.iesteis.gal/api/error/";
+
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<?> handleGeneralException(Exception e) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body("Ocurrió un error inesperado: " + e.getMessage());
+    public ProblemDetail handleGeneralException(Exception e) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR,
+                e.getMessage());
+        problemDetail.setTitle("Ocurrió un error inesperado.");
+        problemDetail.setType(URI.create(BASE_URL + "error-inesperado"));
+        return problemDetail;
+    }
+
+    // Login
+    @ExceptionHandler(BadCredentialsException.class)
+    public ProblemDetail handleBadCredentialsException(BadCredentialsException e) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.UNAUTHORIZED,
+                e.getMessage());
+        problemDetail.setTitle("Credenciales incorrectas.");
+        problemDetail.setType(URI.create(BASE_URL + "credenciales-incorrectas"));
+        return problemDetail;
     }
 
     // Alumnos
     @ExceptionHandler(AlumnoNotFoundException.class)
-    public ResponseEntity<?> handleAlumnoNotFound(AlumnoNotFoundException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    public ProblemDetail handleAlumnoNotFound(AlumnoNotFoundException e) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, e.getMessage());
+        problemDetail.setTitle("Alumno no encontrado.");
+        problemDetail.setType(URI.create(BASE_URL + "alumnos/no-encontrado"));
+        return problemDetail;
     }
 
     @ExceptionHandler(AlumnoForbiddenException.class)
-    public ResponseEntity<?> handleAlumnoAccessDenied(AlumnoForbiddenException e) {
-        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
+    public ProblemDetail handleAlumnoAccessDenied(AlumnoForbiddenException e) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.FORBIDDEN,
+                e.getMessage());
+        problemDetail.setTitle("No tienes permiso para ver los datos de este alumno.");
+        problemDetail.setType(URI.create(BASE_URL + "alumnos/acceso-denegado"));
+        return problemDetail;
     }
 
     // Empresas
     @ExceptionHandler(EmpresaNotFoundException.class)
-    public ResponseEntity<?> handleEmpresaNotFound(EmpresaNotFoundException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    public ProblemDetail handleEmpresaNotFound(EmpresaNotFoundException e) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND,
+                e.getMessage());
+        problemDetail.setTitle("Empresa no encontrada.");
+        problemDetail.setType(URI.create(BASE_URL + "empresas/no-encontrada"));
+        return problemDetail;
     }
 
     @ExceptionHandler(EmpresaForbiddenException.class)
-    public ResponseEntity<?> handleEmpresaForbidden(EmpresaForbiddenException e) {
-        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
+    public ProblemDetail handleEmpresaForbidden(EmpresaForbiddenException e) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.FORBIDDEN,
+                e.getMessage());
+        problemDetail.setTitle("No tienes permiso para ver los datos de esta empresa.");
+        problemDetail.setType(URI.create(BASE_URL + "empresas/acceso-denegado"));
+        return problemDetail;
     }
 
     // Tutores del centro
     @ExceptionHandler(TutorCentroNotFoundException.class)
-    public ResponseEntity<?> handleTutorCentroNotFound(TutorCentroNotFoundException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    public ProblemDetail handleTutorCentroNotFound(TutorCentroNotFoundException e) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND,
+                e.getMessage());
+        problemDetail.setTitle("Tutor del centro no encontrado.");
+        problemDetail.setType(URI.create(BASE_URL + "tutores-centro/no-encontrado"));
+        return problemDetail;
     }
 
     @ExceptionHandler(TutorCentroForbiddenException.class)
-    public ResponseEntity<?> handleTutorCentroForbidden(TutorCentroForbiddenException e) {
-        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
+    public ProblemDetail handleTutorCentroForbidden(TutorCentroForbiddenException e) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.FORBIDDEN,
+                e.getMessage());
+        problemDetail.setTitle("No tienes permiso para ver los datos de este tutor de centro.");
+        problemDetail.setType(URI.create(BASE_URL + "tutores-centro/acceso-denegado"));
+        return problemDetail;
     }
 
     // Usuarios
     @ExceptionHandler(UsuarioNotFoundException.class)
-    public ResponseEntity<?> handleUsuarioNotFound(UsuarioNotFoundException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    public ProblemDetail handleUsuarioNotFound(UsuarioNotFoundException e) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND,
+                e.getMessage());
+        problemDetail.setTitle("Usuario no encontrado.");
+        problemDetail.setType(URI.create(BASE_URL + "usuarios/no-encontrado"));
+        return problemDetail;
     }
 
     @ExceptionHandler(UsuarioForbiddenException.class)
-    public ResponseEntity<?> handleUsuarioForbidden(UsuarioForbiddenException e) {
-        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
+    public ProblemDetail handleUsuarioForbidden(UsuarioForbiddenException e) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.FORBIDDEN,
+                e.getMessage());
+        problemDetail.setTitle("No tienes permiso para ver los datos de este usuario.");
+        problemDetail.setType(URI.create(BASE_URL + "usuarios/acceso-denegado"));
+        return problemDetail;
     }
 
     // FCTs
     @ExceptionHandler(FCTNotFoundException.class)
-    public ResponseEntity<?> handleFCTNotFound(FCTNotFoundException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    public ProblemDetail handleFCTNotFound(FCTNotFoundException e) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND,
+                e.getMessage());
+        problemDetail.setTitle("FCT no encontrada.");
+        problemDetail.setType(URI.create(BASE_URL + "fct/no-encontrada"));
+        return problemDetail;
+
     }
 
     @ExceptionHandler(FCTForbiddenException.class)
-    public ResponseEntity<?> handleFCTForbidden(FCTForbiddenException e) {
-        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
+    public ProblemDetail handleFCTForbidden(FCTForbiddenException e) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.FORBIDDEN,
+                e.getMessage());
+        problemDetail.setTitle("No tienes permiso para ver los datos de esta FCT.");
+        problemDetail.setType(URI.create(BASE_URL + "fct/acceso-denegado"));
+        return problemDetail;
+
     }
 }
