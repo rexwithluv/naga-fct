b8a6467584b0a66b887e100cb64449f190e84525
rexwithluv <aaronaax@gmail.com>
2025-07-25 13:47:22 +0200

    feat(docker): configurar despliegue
    se han hecho ajustes en archivos de configuración así como en archivos docker para preparar la aplicación para un despliegue completo y funcional

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,22 +1,13 @@
 import { PrimeVueResolver } from '@primevue/auto-import-resolver'
 import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
-import dotenv from 'dotenv'
-import { dirname, resolve } from 'node:path'
 import { URL, fileURLToPath } from 'node:url'
 import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// Cargar variables de entorno desde un directorio superior
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-
-dotenv.config({
-  path: resolve(__dirname, '../.env'),
-})
-
 export default defineConfig({
+  base: process.env.VITE_BASE_URL,
   plugins: [
     vue(),
     vueDevTools(),
